ca141d3df5b49d4eb1bafb0da0c052d8c37b12fb
Lukas EAG <lukas@eagproduction.com>
2023-12-22 20:01:15 +0100

    rwd updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,19 @@
-//Navbar 
+//Navbar
 
 const menuOpenBtn = document.querySelector('.nav__mobile-menu-btn--open')
 const menuCloseBtn = document.querySelector('.nav__mobile-menu-btn--close')
 const mobileMenu = document.querySelector('.nav__menu')
 
 menuOpenBtn.addEventListener('click', () => {
-
-  mobileMenu.classList.add('active')
-  menuOpenBtn.classList.add('hide-btn')
-  menuCloseBtn.classList.add('show-btn')
+	mobileMenu.classList.add('active')
+	menuOpenBtn.classList.add('hide-btn')
+	menuCloseBtn.classList.add('show-btn')
 })
 
 menuCloseBtn.addEventListener('click', () => {
-  mobileMenu.classList.remove('active')
-  menuOpenBtn.classList.remove('hide-btn')
-  menuCloseBtn.classList.remove('show-btn')
+	mobileMenu.classList.remove('active')
+	menuOpenBtn.classList.remove('hide-btn')
+	menuCloseBtn.classList.remove('show-btn')
 })
 
 // Gallery carousel
@@ -23,11 +22,12 @@ const gallerySlider = document.querySelector('.showreel__gallery-img-box')
 const buttons = document.querySelectorAll('[data-carousel-button]')
 const galleryBox = document.querySelector('.showreel__gallery-img-box')
 
+
 buttons.forEach(button => {
 	button.addEventListener('click', () => {
 		const offset = button.dataset.carouselButton === 'next' ? 1 : -1
 
-		const imgPos = document.querySelectorAll('.position')
+		const imgPos = document.querySelectorAll('.showreel__position')
 
 		imgPos.forEach((slide, index) => {
 			const indexSlide = gallerySlider.querySelector(`[data-slide-${index}]`)
@@ -39,74 +39,72 @@ buttons.forEach(button => {
 
 			imgPos[newSlide].dataset[`slide-${index}`] = true
 			delete indexSlide.dataset[`slide-${index}`]
-
-			
 		})
-
-    })
+	})
 })
 
 window.addEventListener('resize', () => {
-  let insetWindowWidth = window.innerWidth 
-  let contentWidth =  1600
-  let boxWidth =  insetWindowWidth / contentWidth
-  galleryBox.style.transform =  boxWidth >= 1 ? 'scale(1)' : `scale(${boxWidth})`
+	let insetWindowWidth = window.innerWidth
+	let contentWidth = 1600
+	let boxWidth = insetWindowWidth / contentWidth
+	galleryBox.style.transform = boxWidth >= 1 ? 'scale(1)' : `scale(${boxWidth})`
+})
+window.addEventListener('load', () => {
+  let insetWindowWidth = window.innerWidth
+	let contentWidth = 1600
+	let boxWidth = insetWindowWidth / contentWidth
+	galleryBox.style.transform = `scale(${boxWidth})`
 })
 
-
-// Questions accordeon 
+// Questions accordeon
 
 const accordeon = document.querySelector('.questions__box-lists')
-const accordeonBtns = document.querySelectorAll('.questions__accordeon-btn')
+const accordeonBtns = document.querySelectorAll('.questions__accordeon-box-content')
 
 function openAccordeonItems() {
-	if (this.parentNode.nextElementSibling.classList.contains('show-acc')) {
-		this.parentNode.nextElementSibling.classList.remove('show-acc')
-    this.classList.remove('active')
+	if (this.nextElementSibling.classList.contains('show-acc')) {
+		this.nextElementSibling.classList.remove('show-acc')
+		this.lastElementChild.classList.remove('active')
+	
 	} else {
 		closeAccordeonItems()
-		this.parentNode.nextElementSibling.classList.toggle('show-acc')
-    this.classList.toggle('active')
+		this.nextElementSibling.classList.toggle('show-acc')
+		this.lastElementChild.classList.toggle('active')
 	}
 }
 
 const closeAccordeonItems = () => {
 	const allActiveItems = document.querySelectorAll('.questions__accordeon-info')
 	allActiveItems.forEach(item => item.classList.remove('show-acc'))
-  accordeonBtns.forEach( btn => {
-    btn.classList.remove('active')
-  })
+	accordeonBtns.forEach(btn => {
+		btn.lastElementChild.classList.remove('active')
+	})
 }
 
 const clickOutsideAccordeon = e => {
-    if (e.target.classList.contains('questions__accordeon-btn') || 
-    e.target.classList.contains('questions__accordeon-info') ||
-    e.target.classList.contains('questions__accordeon-info-text')) 
-    return
+	console.log(e.target);
+	if (
+		e.target.classList.contains('target') ||
+		e.target.classList.contains('questions__accordeon-info') ||
+		e.target.classList.contains('questions__accordeon-info-text')
+	)
+		return
 
-    closeAccordeonItems()
-    
+	closeAccordeonItems()
 }
 
 accordeonBtns.forEach(btn => {
-  btn.addEventListener('click', openAccordeonItems)
-  
-}
-  )
+	btn.addEventListener('click', openAccordeonItems)
+})
 
 window.addEventListener('click', clickOutsideAccordeon)
 
+//Contact form slider
 
-
-//Contact form slider 
-
-
-const fromSlider = document.querySelector('#fromSlider');
-const toSlider = document.querySelector('#toSlider');
-const fromInput = document.querySelector('#fromInput');
-const toInput = document.querySelector('#toInput');
-
-
+const fromSlider = document.querySelector('#fromSlider')
+const toSlider = document.querySelector('#toSlider')
+const fromInput = document.querySelector('#fromInput')
+const toInput = document.querySelector('#toInput')
 
 // function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
 //     const [from, to] = getParsed(fromInput, toInput);
@@ -117,9 +115,9 @@ const toInput = document.querySelector('#toInput');
 //     } else {
 //         fromSlider.innerText = from + ` zł`;
 //     }
-   
+
 // }
-    
+
 // function controlToInput(toSlider, fromInput, toInput, controlSlider) {
 //     const [from, to] = getParsed(fromInput, toInput);
 //     fillSlider(fromInput, toInput, '#C6C6C6', '#b60732', controlSlider);
@@ -133,76 +131,72 @@ const toInput = document.querySelector('#toInput');
 // }
 
 function controlFromSlider(fromSlider, toSlider, fromInput) {
-  const [from, to] = getParsed(fromSlider, toSlider);
-  fillSlider(fromSlider, toSlider, '#C6C6C6', '#b60732', toSlider);
-  if (from > to) {
-    fromSlider.value = to;
-    fromInput.innerText = `${to} zł`;
-  } else {
-    fromInput.innerText = `${from} zł`;
-  }
+	const [from, to] = getParsed(fromSlider, toSlider)
+	fillSlider(fromSlider, toSlider, '#C6C6C6', '#b60732', toSlider)
+	if (from > to) {
+		fromSlider.value = to
+		fromInput.innerText = `${to} zł`
+	} else {
+		fromInput.innerText = `${from} zł`
+	}
 }
 
 function controlToSlider(fromSlider, toSlider, toInput) {
-  const [from, to] = getParsed(fromSlider, toSlider);
-  fillSlider(fromSlider, toSlider, '#C6C6C6', '#b60732', toSlider);
-  setToggleAccessible(toSlider);
-  if (from <= to) {
-    toSlider.value = to;
-    toInput.innerText = `${to} zł`;
-  } else {
-    toInput.innerText = `${from} zł`;
-    toSlider.value = from;
-  }
+	const [from, to] = getParsed(fromSlider, toSlider)
+	fillSlider(fromSlider, toSlider, '#C6C6C6', '#b60732', toSlider)
+	setToggleAccessible(toSlider)
+	if (from <= to) {
+		toSlider.value = to
+		toInput.innerText = `${to} zł`
+	} else {
+		toInput.innerText = `${from} zł`
+		toSlider.value = from
+	}
 }
 
 function getParsed(currentFrom, currentTo) {
-  const from = parseInt(currentFrom.value, 10);
-  const to = parseInt(currentTo.value, 10);
-  return [from, to];
+	const from = parseInt(currentFrom.value, 10)
+	const to = parseInt(currentTo.value, 10)
+	return [from, to]
 }
 
 function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
-    const rangeDistance = to.max-to.min;
-    const fromPosition = from.value - to.min;
-    const toPosition = to.value - to.min;
-    controlSlider.style.background = `linear-gradient(
+	const rangeDistance = to.max - to.min
+	const fromPosition = from.value - to.min
+	const toPosition = to.value - to.min
+	controlSlider.style.background = `linear-gradient(
       to right,
       ${sliderColor} 0%,
-      ${sliderColor} ${(fromPosition)/(rangeDistance)*100}%,
-      ${rangeColor} ${((fromPosition)/(rangeDistance))*100}%,
-      ${rangeColor} ${(toPosition)/(rangeDistance)*100}%, 
-      ${sliderColor} ${(toPosition)/(rangeDistance)*100}%, 
-      ${sliderColor} 100%)`;
+      ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
+      ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
+      ${rangeColor} ${(toPosition / rangeDistance) * 100}%, 
+      ${sliderColor} ${(toPosition / rangeDistance) * 100}%, 
+      ${sliderColor} 100%)`
 }
 
 function setToggleAccessible(currentTarget) {
-  const toSlider = document.querySelector('#toSlider');
-  if (Number(currentTarget.value) <= 0 ) {
-    toSlider.style.zIndex = 2;
-  } else {
-    toSlider.style.zIndex = 0;
-  }
+	const toSlider = document.querySelector('#toSlider')
+	if (Number(currentTarget.value) <= 0) {
+		toSlider.style.zIndex = 2
+	} else {
+		toSlider.style.zIndex = 0
+	}
 }
 
+fillSlider(fromSlider, toSlider, '#C6C6C6', '#b60732', toSlider)
+setToggleAccessible(toSlider)
 
-fillSlider(fromSlider, toSlider, '#C6C6C6', '#b60732', toSlider);
-setToggleAccessible(toSlider);
-
-fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
-toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput)
+toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput)
 // fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
 // toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
 
-
-
-
 // Footer current year
 
-const footerYear = document.querySelector('.footer__foot-year');
+const footerYear = document.querySelector('.footer__foot-year')
 const handleCurrentYear = () => {
-    const year = (new Date).getFullYear();
-    footerYear.innerText = year;
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
 }
 
-handleCurrentYear();
+handleCurrentYear()
