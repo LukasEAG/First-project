4338bf76e08a91e866765c7c11b45ea97fbfc594
Lukas EAG <lukas@eagproduction.com>
2024-01-19 20:36:15 +0100

    form update

--- src/js/main.js
+++ src/js/main.js
@@ -160,76 +160,6 @@ accordeonBtns.forEach(btn => {
 
 window.addEventListener('click', clickOutsideAccordeon)
 
-//Contact form slider
-
-// const fromSlider = document.querySelector('#fromSlider')
-// const toSlider = document.querySelector('#toSlider')
-// const fromInput = document.querySelector('#fromInput')
-// const toInput = document.querySelector('#toInput')
-
-// function controlFromSlider(fromSlider, toSlider, fromInput ) {
-
-// 	const [from, to] = getParsed(fromSlider, toSlider)
-// 	fillSlider(fromSlider, toSlider, '#C6C6C6', '#b60732', toSlider)
-// 	if (from > to) {
-// 		fromSlider.value = to
-// 		fromInput.innerText = `${to} `
-// 	} else {
-// 		fromInput.innerText = `${from}  `
-// 	}
-
-// }
-
-// function controlToSlider(fromSlider, toSlider, toInput) {
-
-// 	const [from, to] = getParsed(fromSlider, toSlider)
-// 	fillSlider(fromSlider, toSlider, '#C6C6C6', '#b60732', toSlider)
-// 	setToggleAccessible(toSlider)
-// 	if (from <= to) {
-// 		toSlider.budget = to
-// 		toInput.innerText = `${to} `
-// 	} else {
-// 		toInput.innerText = `${from} `
-// 		toSlider.value = from
-// 	}
-
-// }
-
-// function getParsed(currentFrom, currentTo) {
-// 	const from = parseInt(currentFrom.value, 10)
-// 	const to = parseInt(currentTo.value, 10)
-// 	return [from, to]
-// }
-
-// function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
-// 	const rangeDistance = to.max - to.min
-// 	const fromPosition = from.value - to.min
-// 	const toPosition = to.value - to.min
-// 	controlSlider.style.background = `linear-gradient(
-//       to right,
-//       ${sliderColor} 0%,
-//       ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
-//       ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
-//       ${rangeColor} ${(toPosition / rangeDistance) * 100}%,
-//       ${sliderColor} ${(toPosition / rangeDistance) * 100}%,
-//       ${sliderColor} 100%)`
-// }
-
-// function setToggleAccessible(currentTarget) {
-// 	const toSlider = document.querySelector('#toSlider')
-// 	if (Number(currentTarget.value) <= 0) {
-// 		toSlider.style.zIndex = 2
-// 	} else {
-// 		toSlider.style.zIndex = 0
-// 	}
-// }
-
-// fillSlider(fromSlider, toSlider, '#C6C6C6', '#b60732', toSlider)
-// setToggleAccessible(toSlider)
-
-// fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput)
-// toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput)
-
 // Contact form budget
 
 const budgetBoxFrom = document.querySelector('.contact__budget-box--from')
@@ -350,6 +280,34 @@ langBtns.forEach(btn =>
 	})
 )
 
+//Contact form input list 
+
+const btnFileUpload = document.querySelector('.contact__attachments-btn')
+const hideFileInput = document.querySelector('.contact__attachments-input')
+const fileUploadLabel = document.querySelector('.contact__attachments-label')
+const labelOutput = []
+
+
+btnFileUpload.addEventListener('click', (e) => {
+	e.preventDefault()
+	hideFileInput.click()
+	
+})
+
+hideFileInput.addEventListener('change', () => {
+
+	const filenameList = Array.prototype.map.call(hideFileInput.files, file =>  {
+        return file.name;
+      });
+	  fileUploadLabel.innerText = 'Wybrane pliki:'
+
+	  filenameList.map( file => {
+		const fileListItem = document.createElement('li')
+		fileListItem.innerText = file
+		fileUploadLabel.appendChild(fileListItem)
+	  })
+		 
+})
 // Footer current year
 
 const footerYear = document.querySelector('.footer__foot-year')
